f1225e80fd0e2d60e32c39f7f8b3c53b481eab45
MansoorSN <snmansoor25@gmail.com>
2021-09-21 16:30:32 -0400

    converted to maven project

---
+++ src/e1/ErrorValidatorTest.java
@@ -0,0 +1,23 @@
+package e1;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class ErrorValidatorTest {
+
+	@Test
+	public void testErrorValidator() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidatenot() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidatetan() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
